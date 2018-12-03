0c0bb80467b24ee2405b6903c9666ce1afa24b84
dwgitdlee@outlook.kr <dwgitdlee@outlook.kr>
2018-12-03 12:40:53 +0900

    8진수 16진수 출력

--- CH2/2-5.c
+++ CH2/2-5.c
@@ -4,6 +4,12 @@ int main(void) {
 	printf("%d\n", 12);  //10진수
 	printf("%d\n", 014); //8진수
 	printf("%d\n", 0xc); //16진수
+	
+	printf("\n");
+
+	printf("%d\n", 12);  //10진수
+	printf("8진수 출력:%o\n", 12); //8진수 출력
+	printf("16진수 출력:%x\n", 12); //16진수 출력
 
 	return 0;
-}수
+}
