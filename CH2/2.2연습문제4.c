60b7a3c77b5084eb5ef32b9ccdb9b55ae7e92d1e
dwgitdlee@outlook.kr <dwgitdlee@outlook.kr>
2018-12-04 09:25:37 +0900

    연습문제

---
+++ CH2/2.2연습문제4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	/*
+	int number = 32165;
+	string name = "홍길동";
+	const char grade = 'A';
+	*/
+	printf("학번: %d\n", 32165);
+	printf("이름: %s\n", "홍길동");
+	printf("학점: %c", 'A');
+
+	return 0;
+}
